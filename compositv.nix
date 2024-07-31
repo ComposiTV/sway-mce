b8adcd1450e5f49b6b57a789f389e92609ef8a93
SED4906 <37986786+SED4906@users.noreply.github.com>
2024-07-31 08:04:19 -0700

    Update compositv.nix

--- compositv.nix
+++ compositv.nix
@@ -1,12 +1,6 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 { config, lib, pkgs, ... }:
 
 let
-
-#  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
 
   ctv-logo = pkgs.fetchurl {
     url = "https://4906.org/m/compositv.png";
@@ -44,7 +38,7 @@ let
     destination = "/bin/send-key";
     executable = true;
     text = ''
-      echo key $1 | dotool
+      echo key $1 | dotoolc
     '';
   };
 
@@ -62,7 +56,7 @@ let
     destination = "/bin/click-mouse";
     executable = true;
     text = ''
-      echo click $1 | dotool
+      echo click $1 | dotoolc
     '';
   };
 
@@ -71,16 +65,13 @@ let
     destination = "/bin/toggle-osk";
     executable = true;
     text = ''
-      kill -n 34 `pidof wvkbd-mobintl` # SIGRTMIN
+      kill -s 34 `pidof wvkbd-mobintl` # SIGRTMIN
     '';
   };
 
 in
 
 {
-#  imports = [
-#    (import "${home-manager}/nixos")
-#  ];
 
   security.polkit = {
     extraConfig = ''
@@ -106,12 +97,6 @@ in
       };
     };
   };
-
-#  nixpkgs.config.packageOverrides = pkgs: {
-#    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
-#      inherit pkgs;
-#    };
-#  };
 
   boot.plymouth = {
     enable = true;
@@ -143,8 +128,6 @@ in
     packages = with pkgs; [
       alacritty
 #      wev
-#      firefox
-#      nur.repos.rycee.firefox-addons.ublock-origin
       minitube
       vlc
     ];
@@ -191,7 +174,7 @@ set $term alacritty
 set $menu fuzzel | xargs swaymsg exec --
 font monospace 12
 seat seat0 xcursor_theme "Adwaita" 32
-output * scale 2
+output * scale 4
 output * bg ${ctv-logo} center
 bindsym Mod4+Mod1+n kill
 bindsym Mod4+Mod1+m exec $menu & swaymsg mode "app"
@@ -271,27 +254,6 @@ exec dotoold
     operation = "boot";
   };
 
-#  home-manager.users.tv = {
-#    home.pointerCursor = {
-#      name = "Adwaita";
-#      package = pkgs.gnome.adwaita-icon-theme;
-#      size = 32;
-#      x11 = {
-#        enable = true;
-#        defaultCursor = "Adwaita";
-#      };
-#    };
-#    home.stateVersion = "24.05";
-#  };
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
   # List services that you want to enable:
 
   services.dbus.enable = true;
@@ -320,38 +282,6 @@ exec dotoold
   services.udev.extraRules = ''
     KERNEL=="uinput", GROUP="input", MODE="0660", OPTIONS+="static_node=uinput"
   '';
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  # system.stateVersion = "24.05"; # Did you read the comment?
 
 }
 
